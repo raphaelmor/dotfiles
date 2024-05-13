91c7eba0b933e14744d593dd6088b3ba909292bc
Raphaël Mor <rmor@apple.com>
2024-05-13 19:53:48 +0200

    Update .config/alacritty/alacritty.toml Add .config/nvim/LICENSE Add .config/nvim/README.md Add .config/nvim/.gitignore Add .config/nvim/.neoconf.json Add .config/nvim/init.lua Add .config/nvim/lazy-lock.json Add .config/nvim/lazyvim.json Add .config/nvim/lua/config/autocmds.lua Add .config/nvim/lua/config/keymaps.lua Add .config/nvim/lua/config/lazy.lua Add .config/nvim/lua/config/options.lua Add .config/nvim/lua/plugins/example.lua Add .config/nvim/lua/plugins/gruvbox.lua Add .config/nvim/stylua.toml Update install_tools.sh

---
+++ dot_config/nvim/lua/plugins/gruvbox.lua
@@ -0,0 +1,22 @@
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+  {
+    "telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
+    },
+  },
+}
--- run_onchange_before_install_tools.sh
+++ run_onchange_before_install_tools.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# install tools
 brew bundle --no-lock --file=/dev/stdin <<EOF
 tap "homebrew/cask-fonts"
 cask "font-meslo-lg-nerd-font"
@@ -10,9 +10,26 @@ brew "ripgrep"
 brew "fd"
 brew "fzf"
 brew "neovim"
+brew "neofetch"
+brew "zsh-syntax-highlighting"
+brew "zoxide"
 
 EOF
-rm -rf  ~/.config/zsh/powerlevel10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.config/zsh/powerlevel10k 
 
+#install powerlevel10k
+mv ~/.config/zsh/powerlevel10k{,.bak}
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.config/zsh/powerlevel10k
+
+#make alacritty look better
 defaults write org.alacritty AppleFontSmoothing -int 0
+
+# Setup LazyVim
+# required
+mv ~/.config/nvim{,.bak}
+# optional but recommended
+mv ~/.local/share/nvim{,.bak}
+mv ~/.local/state/nvim{,.bak}
+mv ~/.cache/nvim{,.bak}
+
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git
