f41ff94dd853e5858d7958862ac24c7ab915c33f
Raphaël Mor <rmor@apple.com>
2024-05-13 18:21:01 +0200

    Add .config/alacritty/alacritty.toml Update install_tools.sh

--- run_onchange_before_install_tools.sh
+++ run_onchange_before_install_tools.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 brew bundle --no-lock --file=/dev/stdin <<EOF
+cask "alacritty"
 brew "eza"
+brew "lazygit"
+brew "ripgrep"
+brew "fd"
+brew "fzf"
 brew "neovim"
+
 EOF
 rm -rf  ~/.config/zsh/powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.config/zsh/powerlevel10k 
