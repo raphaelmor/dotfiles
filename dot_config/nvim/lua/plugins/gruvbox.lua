9b0e7ef6db33fa9bf045437f3f9fa22286586625
Raphaël Mor <raphael.mor@gmail.com>
2024-07-09 15:04:56 +0200

    update

--- dot_config/nvim/lua/plugins/gruvbox.lua
+++ dot_config/nvim/lua/plugins/gruvbox.lua
@@ -1,26 +1,23 @@
 return {
   -- add gruvbox
-  { "ellisonleao/gruvbox.nvim", priority = 1000, config = true },
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = function()
+      require("gruvbox").setup({
+        overrides = {
+          Directory = { link = "GruvboxBlueBold" }
+
+        }
+      })
+    end,
+  },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "gruvbox",
-    },
-  },
-  {
-    "lualine.nvim",
-    opts = function() end,
-  },
-  {
-    "telescope.nvim",
-    dependencies = {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build = "make",
-      config = function()
-        require("telescope").load_extension("fzf")
-      end,
     },
   },
 }
