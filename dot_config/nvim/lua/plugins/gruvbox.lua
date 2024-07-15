f9678de4afef8b480eb1d4225c978bae0f55bbad
Raphaël Mor <raphael.mor@gmail.com>
2024-07-15 15:47:51 +0200

    Update .config/alacritty/alacritty.toml Update .config/nvim/lazy-lock.json Update .config/nvim/lazyvim.json Update .config/nvim/lua/plugins/gruvbox.lua

--- dot_config/nvim/lua/plugins/gruvbox.lua
+++ dot_config/nvim/lua/plugins/gruvbox.lua
@@ -1,23 +1,47 @@
+-- return {
+--   -- add gruvbox
+--   {
+--     "ellisonleao/gruvbox.nvim",
+--     priority = 1000,
+--     config = function()
+--       require("gruvbox").setup({
+--         overrides = {
+--           Directory = { link = "GruvboxBlueBold" }
+--
+--         }
+--       })
+--     end,
+--   },
+--
+--   -- Configure LazyVim to load gruvbox
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "gruvbox",
+--     },
+--   },
+-- }
+--
 return {
-  -- add gruvbox
   {
-    "ellisonleao/gruvbox.nvim",
+    "sainnhe/gruvbox-material",
+    lazy = false,
     priority = 1000,
     config = function()
-      require("gruvbox").setup({
-        overrides = {
-          Directory = { link = "GruvboxBlueBold" }
-
-        }
-      })
-    end,
+      -- Optionally configure and load the colorscheme
+      -- directly inside the plugin declaration.
+      vim.g.gruvbox_material_enable_italic = true
+      vim.g.gruvbox_material_background = 'hard'
+      vim.cmd.colorscheme('gruvbox-material')
+    end
   },
-
-  -- Configure LazyVim to load gruvbox
   {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "gruvbox",
+    {
+      "LazyVim/LazyVim",
+      opts = {
+        colorscheme = "gruvbox-material",
+      },
     },
-  },
+
+  }
 }
