81f440db2a29245fbe0583751d72a3e2d769890a
Raphaël Mor <raphael.mor@gmail.com>
2024-12-28 22:09:05 +0100

    Update .config/alacritty/alacritty.toml Update .config/nvim/lazy-lock.json Update .config/nvim/lazyvim.json Update .config/nvim/lua/plugins/gruvbox.lua

--- dot_config/nvim/lua/plugins/gruvbox.lua
+++ dot_config/nvim/lua/plugins/gruvbox.lua
@@ -1,6 +1,7 @@
 return {
   {
     "sainnhe/gruvbox-material",
+    enabled = true,
     lazy = false,
     priority = 1000,
     config = function()
@@ -8,8 +9,11 @@ return {
       -- directly inside the plugin declaration.
       vim.g.gruvbox_material_enable_italic = true
       vim.g.gruvbox_material_background = "medium"
-      vim.g.gruvbox_material_transparent_background = true
+      vim.g.gruvbox_material_transparent_background = 1
       vim.g.gruvbox_material_enable_bold = true
+      vim.g.gruvbox_material_diagnostic_text_highlight = 1
+      vim.g.gruvbox_material_diagnostic_line_highlight = 0
+      vim.g.gruvbox_material_diagnostic_virtual_text = "colored"
       vim.cmd.colorscheme("gruvbox-material")
     end,
   },
